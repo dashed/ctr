166adae6042b706057739450525ea087287eb556
David Burleson <davidburleson@gmail.com>
2023-03-16 12:23:52 +0000

    #173 refactored place controller using service->repo for db

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -5,6 +5,7 @@ export * from './hood/hood.service';
 export * from './member/member.service';
 export * from './message/message.service';
 export * from './object-instance/object-instance.service';
+export * from './place/place.service';
 export * from './wallet/wallet.service';
 export * from './role/role.service';
 export * from './role-assignment/role-assignment.service';
